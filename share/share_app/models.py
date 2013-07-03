87f286759a11495af2ca58877fb7fe6df94ccb84
Roni London <ronilon@cs.huji.ac.il>
2013-07-03 15:37:01 +0300

    fixed name issue in profile

--- share/share_app/models.py
+++ share/share_app/models.py
@@ -7,11 +7,6 @@ class Profile(models.Model):
 	hobbies = models.CharField(max_length=200)
 	school = models.CharField(max_length=50)
 	birthday = models.DateField()
-	username = models.CharField(max_length=200)
-	email = models.CharField(max_length=200)
-	password = models.CharField(max_length=200)
-	first_name = models.CharField(max_length=200)
-	last_name = models.CharField(max_length=200)
 	user = models.OneToOneField(User)
 
 	
