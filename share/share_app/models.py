0dd9affd97d23ab67b9c7f699cb4d95deabb4d5b
Roni London <ronilon@cs.huji.ac.il>
2013-07-03 12:20:26 +0300

    fixed buttons and changed the profile to be dynamic

--- share/share_app/models.py
+++ share/share_app/models.py
@@ -4,10 +4,6 @@ from django.contrib.auth import authenticate
 # Create your models here.
 
 
-class Users(models.Model):
-	firstname = models.CharField(max_length=15)
-	lastname = models.CharField(max_length=20)
-	emailaddress = models.CharField(max_length=32)
 	
 
 	
